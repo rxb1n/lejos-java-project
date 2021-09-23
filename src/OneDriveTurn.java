427658d14ec889bbccbfcae10787043f7700d92d
Robin Bruch <robinbruch@gmx.de>
2021-09-23 14:38:09 +0200

    OneDriveTrun

---
+++ src/OneDriveTurn.java
@@ -0,0 +1,18 @@
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.robotics.RegulatedMotor;
+import lejos.utility.Delay;
+
+
+public class OneDriveTurn {
+	
+	static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+	static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
+
+	public static void main(String[] args) {
+		
+		  rightMotor.synchronizeWith(new RegulatedMotor[] {leftMotor});
+
+		  
+	}
+}
