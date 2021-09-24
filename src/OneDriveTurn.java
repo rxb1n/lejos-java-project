efcfc5ac18f29fa70359a17a2e496d1a0684de58
Robin Bruch <robinbruch@gmx.de>
2021-09-24 11:28:02 +0200

    OneDriveTurn is ready

--- src/OneDriveTurn.java
+++ src/OneDriveTurn.java
@@ -1,3 +1,4 @@
+
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.robotics.RegulatedMotor;
@@ -9,8 +10,7 @@ public class OneDriveTurn {
 	static RegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.B);
 	static RegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.C);
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		  rightMotor.synchronizeWith(new RegulatedMotor[] {leftMotor});
 
 		  rightMotor.startSynchronization();
